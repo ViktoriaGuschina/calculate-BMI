af690655c040e2a47f37207130efc507a6dc24a1
VIKS NEW <guschina.viktoria@gmail.com>
2024-04-20 22:53:18 +0300

    fix squareHeight after review

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public int calculate(double height, int weight) {
-        double squareheight = height * height;
-        int bmi = (int) (weight / squareheight);
+        double squareHeight = height * height;
+        int bmi = (int) (weight / squareHeight);
         return bmi;
     }
 }
